a1d601a2a798ac8bc2ae26d9be8a6d8d2b525bad
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2021-03-13 17:53:45 -0800

    Add test image function

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# raytracing-one-weekend
+# Ray Tracing in one weekend - Python
+
+Running throughthe guide at https://raytracing.github.io/books/RayTracingInOneWeekend.html 
+to create a raytracer.
---
+++ src/raytracing_one_weekend/main.py
@@ -0,0 +1,29 @@
+"""
+Main entry point for renderer functionality
+"""
+
+from PIL import Image, ImageDraw
+
+IMG_HEIGHT = 90
+IMG_WIDTH = 160
+
+
+def generate_test_image():
+    """
+    Generate a test image.
+    """
+
+    image = Image.new("RGB", (IMG_WIDTH, IMG_HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    for row in range(IMG_HEIGHT):
+        for column in range(IMG_WIDTH):
+            r = int(255 * (column / (IMG_WIDTH - 1)))
+            g = int(255 * (row / (IMG_HEIGHT - 1)))
+            b = int(255 * 0.25)
+            draw.point((column, row), fill=(r, g, b))
+
+    image.show()
+
+
+generate_test_image()
