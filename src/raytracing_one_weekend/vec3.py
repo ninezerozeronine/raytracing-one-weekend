9e88f82566227fb893ab0c0b54ed148ee191eefa
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2021-03-21 18:55:14 -0700

    Add some docstrings to Vec3.

--- src/raytracing_one_weekend/vec3.py
+++ src/raytracing_one_weekend/vec3.py
@@ -70,7 +70,10 @@ class Vec3():
 
     def __add__(self, other):
         """
+        Add two Vec3s together.
 
+        E.g.::
+            vec3 = vec1 + vec2
         """
         if isinstance(other, Vec3):
             return Vec3(
@@ -85,7 +88,10 @@ class Vec3():
 
     def __iadd__(self, other):
         """
+        Add another Vec3 to the current one in place.
 
+        E.g.::
+            vec1 += vec2
         """
         if isinstance(other, Vec3):
             self._e0 += other._e0
@@ -99,7 +105,10 @@ class Vec3():
 
     def __eq__(self, other):
         """
+        Check for equality between two Vec3s.
 
+        E.g.::
+            vec1 == vec2
         """
         if isinstance(other, Vec3):
             tolerance = 0.0001
@@ -115,7 +124,17 @@ class Vec3():
 
     def _validate_key(self, key):
         """
+        Check if key is valid.
 
+        The key is used when indexing into a Vec3. E.g.::
+            first_elem = my_vec3[0]
+
+        Args:
+            key (int): The key to check.
+
+        Raises:
+            TypeError: If the key isn't an int.
+            IndexError: If the index is out of range.
         """
         if not isinstance(key, int):
             raise(TypeError("Key is not an int"))
@@ -125,13 +144,19 @@ class Vec3():
 
     def _validate_value(self, value):
         """
+        Check if a value is valid for a Vec3.
 
+        Raises:
+            ValueError: If the value isn't a number.
         """
         if not isinstance(value, numbers.Number):
             raise ValueError("Value passed is not a number.")
 
     @property
     def x(self):
+        """
+        Access the first element as x.
+        """
         return self._e0
 
     @x.setter
@@ -141,6 +166,9 @@ class Vec3():
 
     @property
     def y(self):
+        """
+        Access the second element as y.
+        """
         return self._e1
 
     @x.setter
@@ -150,6 +178,9 @@ class Vec3():
 
     @property
     def z(self):
+        """
+        Access the third element as z.
+        """
         return self._e2
 
     @x.setter
@@ -159,12 +190,29 @@ class Vec3():
 
     def length(self):
         """
+        The length of the Vec3.
 
+        This assumes that the x, y and z values are in an orthonormal
+        coordinate system. (I.e. all 3 axes are unit length and at right
+        angles to each other.)
+
+        Returns:
+            float: The length of the Vec3.
         """
         return math.sqrt(self.length_squared())
 
     def length_squared(self):
         """
+        The length of the Vec3 squared.
 
+        This assumes that the x, y and z values are in an orthonormal
+        coordinate system. (I.e. all 3 axes are unit length and at right
+        angles to each other.)
+
+        This is useful as it's faster to compare lengths of vectors
+        because you don't have to calculate the square root.
+
+        Returns:
+            float: The length of the Vec3 squared.
         """
         return (self._e0 ** 2) + (self._e1 ** 2) + (self._e2 ** 2)
