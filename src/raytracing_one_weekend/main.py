b6e360823e51b337ced77450be021657031c505b
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2021-04-02 12:24:52 -0700

    Add ray sphere maffs notes

--- src/raytracing_one_weekend/main.py
+++ src/raytracing_one_weekend/main.py
@@ -33,7 +33,7 @@ def generate_test_image():
 
 def generate_image_from_data(img_data):
     """
-
+    Write out an image to disk.
     """
 
     image = Image.new("RGB", (IMG_WIDTH, IMG_HEIGHT))
@@ -58,6 +58,9 @@ def generate_image_from_data(img_data):
 
 
 def render():
+    """
+    Do the rendering of the image.
+    """
     viewport_width = 2.0
     viewport_height = viewport_width * (IMG_HEIGHT/IMG_WIDTH)
     viewport_vertical = Vec3(0, viewport_height, 0)
@@ -114,6 +117,75 @@ def get_ray_colour(ray):
 
     # Lerp between white and blue based on mapped Y
     return (1.0 - t) * HORIZON_COLOUR + t * SKY_COLOUR
+
+
+def ray_sphere_intersection(centre, radius, ray):
+    """
+    Check if a ray intersects a sphere.
+
+    A point is on a sphere if the length of the vector from the centre
+    of the sphere to a point on the sphere is equal to the radius of
+    the sphere.
+
+    If you take the dot product of a vector with itself, this is the
+    length squared.
+
+    If R is the vector from the center to a point on the sphere, and r
+    is the radius then if R.R = r^2 the point is on the sphere.
+
+    R is the vector from the centre (C) to a point (P) this means
+    R = P - C (i.e. centre to origin, then origin to point).
+
+    So if (P - C).(P - C) = r^2, the point is on the sphere.
+
+    We want to find if a point on a ray intersects the sphere. We can
+    plug in the point on ray representation (Pt on Ray = O + tD, or
+    origin + multiplier of the direction) to the equation above:
+
+    (O + tD - C).(O + tD - C) = r^2
+
+    Exapnd this out and we get:
+
+    O.O + t(D.O) - C.O
+
+    + t(D.O) + t^2(D.D) - t(C.D)
+
+    - C.O - t(C.D) + C.C = r^2
+
+    Collecting terms we get:
+
+    O.O + 2t(D.O) - 2(C.O) + t^2(D.D) - 2t(C.D) + C.C = r^2
+
+    Note that (O - C).(O - C) = O.O - 2(C.O) + C.C so we can simplify
+    to:
+
+    2t(D.O) + t^2(D.D) -2t(C.D) + (O - C).(O - C) = r^2
+
+    Collapsing the 2t factors and re-arranging a bit:
+
+    t^2(D.D) + 2tD.(O - C) + (O - C).(O - C) = r^2
+
+    Re arrange to equal zero and we have a quadratic in terms of t!
+
+    t^2(D.D) + 2tD.(O - C) + (O - C).(O - C) - r^2 = 0
+
+    Where:
+
+    A = D.D
+    B = D.(O - C)
+    C = (O - C).(O - C) - r^2
+
+    Using our old friend the quadratic equation::
+
+        x = (-B +/- sqrt(B^2 - 4AC)) / (2A)
+
+    We know that if B^2 - 4AC is less than 0 the equation has no
+    roots - or - the ray doesn't intersect the sphere!
+
+
+    """
+
+
 
 
 def main():
